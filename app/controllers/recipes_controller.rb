769e7827dfeffa77a5edd2167e9249ef30b27237
jugtuttle <jugtuttle@gmail.com>
2020-02-11 10:51:01 -0800

    recipe create working but need to add logic for user and categories

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,6 +9,39 @@ class RecipesController < ApplicationController
         @recipe = Recipe.find(params[:id])
         render json: @recipe
     end 
+
+    def create
+        # byebug 
+        ### need to add logic to add user to recipe 
+        # @user = User.find(params[:user])
+        @user = User.find(1)
+        @recipe = Recipe.new 
+        @recipe.title = params[:title]
+        @recipe.img = params[:image]
+        @recipe.directions = params[:directions]
+        @recipe.area = params[:area]
+        @recipe.category = params[:category]
+        @user_recipe = UserRecipe.new 
+        ing_array = [] 
+        if @recipe.save
+            @user_recipe.recipe = @recipe 
+            @user_recipe.user = @user
+            params[:ingredients].each do |ing|
+                @ingredient = Ingredient.find_or_create_by(ing_name: ing[:ingName])
+                if @ingredient.save 
+                    ing_array.push(@ingredient)
+                @recipe_ingredient = RecipeIngredient.new
+                @recipe_ingredient.recipe = @recipe
+                @recipe_ingredient.ingredient = @ingredient
+                @recipe_ingredient.amount = ing[:amount]
+                @recipe_ingredient.save 
+                end 
+            end
+            render json: {recipe: @recipe, ingredients: ing_array}
+        end 
+
+
+    end
     
     # private 
     # def recipe_params
